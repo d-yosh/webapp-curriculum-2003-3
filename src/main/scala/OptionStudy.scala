35e7d4d7b99f6c05c4026958e75868eb3929bec1
d-yosh <daicedaice87@gmail.com>
2020-01-15 21:40:49 +0900

    OptionStudy を実装

--- src/main/scala/OptionStudy.scala
+++ src/main/scala/OptionStudy.scala
@@ -1,11 +1,17 @@
 object OptionStudy {
 
   trait Option[+A] {
-    def map[B](f: A => B): Option[B] = ???
+    def map[B](f: A => B): Option[B] = this match {
+      case Some(get) => Some(f(get))
+      case None => None
+    }
 
-    def getOrElse[B >: A](default: => B): B = ???
+    def getOrElse[B >: A](default: => B): B = this match {
+      case Some(get) => get
+      case None => default
+    }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
   }
 
   case class Some[+A](get: A) extends Option[A]
